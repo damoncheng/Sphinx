d87fe3294d56a630110f59b764c6d09575c53604
root <root@damoncheng.www.damoncheng.com>
2018-10-12 23:56:41 +0800

    adjust

--- source/conf.py
+++ source/conf.py
@@ -45,7 +45,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'sphinx'
+project = 'django_sim'
 copyright = '2018, damoncheng'
 
 # The version info for the project you're documenting, acts as replacement for
